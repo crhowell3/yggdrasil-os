4d4cd549ea041331e4db69362c2b6f63dbbc6d4b
Cameron Howell <jango.djarin@proton.me>
2025-04-10 23:46:38 -0500

    Added run and debug scripts, fixed bootloader errors

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -118,6 +118,7 @@ main:
     mov si, startup_msg
     call puts
 
+    cli
     hlt
 
 ; ==============
@@ -220,11 +221,11 @@ disk_read:
 .done:
     popa
 
-    push di
-    push dx
-    push cx
-    push bx
-    push ax
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
     ret
 
 ;
