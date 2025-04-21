1d6173df9ad17eca29a37de5173443564868a044
Cameron Howell <jango.djarin@proton.me>
2025-04-20 23:06:07 -0500

    Added printf implementation

--- src/bootloader/stage2/stdio.c
+++ src/bootloader/stage2/stdio.c
@@ -11,3 +11,193 @@ void puts(const char* str) {
         str++;
     }
 }
+
+enum PrintfState {
+    kPrintfStateNormal,
+    kPrintfStateLength,
+    kPrintfStateLengthShort,
+    kPrintfStateLengthLong,
+    kPrintfStateSpec
+};
+
+enum PrintfLength {
+    kPrintfLengthDefault,
+    kPrintfLengthShortShort,
+    kPrintfLengthShort,
+    kPrintfLengthLong,
+    kPrintfLengthLongLong
+};
+
+int* printf_number(int* argp, int length, bool sign, int radix);
+
+void _cdecl printf(const char* fmt, ...) {
+    int* argp = (int*)&fmt;
+    int state = kPrintfStateNormal;
+    int length = kPrintfLengthDefault;
+    int radix = 10;
+    bool sign = false;
+
+    argp++;
+
+    while (*fmt) {
+        switch (state) {
+            case kPrintfStateNormal:
+                switch (*fmt) {
+                    case '%':
+                        state = kPrintfStateLength;
+                        break;
+                    default:
+                        putc(*fmt);
+                        break;
+                }
+                break;
+            case kPrintfStateLength:
+                switch (*fmt)
+                {
+                case 'h':
+                    length = kPrintfLengthShort;
+                    state = kPrintfStateLengthShort;
+                    break;
+                case 'l':
+                    length = kPrintfLengthLong;
+                    state = kPrintfStateLengthLong;
+                    break;
+                default:
+                    goto PRINTF_STATE_SPEC_;
+                }
+                break;
+            case kPrintfStateLengthShort:
+                if (*fmt == 'h') {
+                    length = kPrintfLengthShortShort;
+                    state = kPrintfStateSpec;
+                } else {
+                    goto PRINTF_STATE_SPEC_;
+                }
+                break;
+            case kPrintfStateLengthLong:
+                if (*fmt == 'l') {
+                    length = kPrintfLengthLongLong;
+                    state = kPrintfStateSpec;
+                } else {
+                    goto PRINTF_STATE_SPEC_;
+                }
+                break;
+            case kPrintfStateSpec:
+            PRINTF_STATE_SPEC_:
+                switch (*fmt) {
+                    case 'c':
+                        putc((char)*argp);
+                        argp++;
+                        break;
+                    case 's': 
+                        puts(*(char**)argp);
+                        argp++;
+                        break;
+                    case '%':
+                        putc('%');
+                        break;
+                    case 'd':
+                    case 'i':
+                        radix = 10;
+                        sign = true;
+                        argp = printf_number(argp, length, sign, radix);
+                        break;
+                    case 'u':
+                        radix = 10;
+                        sign = false;
+                        argp = printf_number(argp, length, sign, radix);
+                        break;
+                    case 'X':
+                    case 'x':
+                    case 'p':
+                        radix = 16;
+                        sign = false;
+                        argp = printf_number(argp, length, sign, radix);
+                        break;
+                    case 'o':
+                        radix = 8;
+                        sign = false;
+                        argp = printf_number(argp, length, sign, radix);
+                        break;
+                    default:
+                        break;
+                }
+                state = kPrintfStateNormal;
+                length = kPrintfLengthDefault;
+                radix = 10;
+                sign = false;
+                break;
+        }
+        fmt++;
+    }
+}
+
+const char kHexChars[] = "0123456789abcdef";
+
+int* printf_number(int* argp, int length, bool sign, int radix) {
+    char buffer[32];
+    unsigned long long number;
+    int number_sign = 1;
+    int pos = 0;
+
+    switch (length) {
+        case kPrintfLengthShortShort:
+        case kPrintfLengthShort:
+        case kPrintfLengthDefault:
+            if (sign) {
+                int n = *argp;
+                if (n < 0) {
+                    n = -n;
+                    number_sign = -1;
+                }
+                number = (unsigned long long)n;
+            } else {
+                number = *(unsigned int*)argp;
+            }
+            argp++;
+            break;
+        case kPrintfLengthLong:
+            if (sign) {
+                long int n = *(long int*)argp;
+                if (n < 0) {
+                    n = -n;
+                    number_sign = -1;
+                }
+                number = (unsigned long long)n;
+            } else {
+                number = *(unsigned long int*)argp;
+            }
+            argp += 2;
+            break;
+        case kPrintfLengthLongLong:
+                    if (sign) {
+                long long int n = *(long long int*)argp;
+                if (n < 0) {
+                    n = -n;
+                    number_sign = -1;
+                }
+                number = (unsigned long long)n;
+            } else {
+                number = *(unsigned long long*)argp;
+            }
+            argp += 2;
+            break;
+    }
+
+    // Convert number to ASCII
+    do {
+        uint32_t rem;
+        x86_div64_32(number, radix, &number, &rem);
+        buffer[pos++] = kHexChars[rem];
+    } while (number > 0);
+
+    if (sign && number_sign < 0) {
+        buffer[pos++] = '-';
+    }
+
+    while (--pos >= 0) {
+        putc(buffer[pos]);
+    }
+
+    return argp;
+}
--- src/bootloader/stage2/x86.asm
+++ src/bootloader/stage2/x86.asm
@@ -2,6 +2,48 @@ bits 16
 
 section _TEXT class=CODE
 
+;
+; void _cdecl x86_div64_32(uint64_t dividend, uint32_t divisor, uint64_t* quotient_out, uint32_t* remainder_out);
+;
+global _x86_div64_32
+_x86_div64_32:
+
+    ; Make new call frame
+    push bp             ; Save old call frame
+    mov bp, sp          ; Initialize new call frame
+
+    push bx
+
+    ; Divide upper 32 bits
+    mov eax, [bp + 8]   ; eax <- upper 32 bits of dividend
+    mov ecx, [bp + 12]  ; ecx <- divisor
+    xor edx, edx
+    div ecx
+
+    ; Store upper 32 bits of quotient
+    mov bx, [bp + 16]
+    mov [bx + 4], eax
+
+    ; Divide lower 32 bits
+    mov eax, [bp + 4]
+    div ecx
+
+    ; Store results
+    mov [bx], eax
+    mov bx, [bp + 18]
+    mov bx, [edx]
+
+    pop bx
+
+    ; Restore old call frame
+    mov sp, bp
+    pop bp
+    ret
+
+;
+; int 10h ah=0Eh
+; args: character, page
+;
 global _x86_Video_WriteCharTeletype
 _x86_Video_WriteCharTeletype:
     push bp
